27867d465451b19f152237dd2dedf59b448554ff
Eugene <chromigo@gmail.com>
2014-03-10 16:58:58 +0400

    Добавил заглушку класса кластеризации учебных направлений

---
+++ OptimalEducation/Logic/Clusterizer/EducationLineClusterizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OptimalEducation.Logic.Clusterizer
+{
+    /// <summary>
+    /// Результаты кластеризации для учебного направления. 
+    /// </summary>
+    public class EducationLineClusterizer
+    {
+    }
+}
