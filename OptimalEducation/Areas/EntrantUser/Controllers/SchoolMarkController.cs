99023a3f30e757205f53c57592517533ab8b30b9
Eugene <chromigo@gmail.com>
2014-03-16 17:54:28 +0400

    Добавил возможность заполнять и просматривать школьные оценки абитуриента + валидация в модели

---
+++ OptimalEducation/Areas/EntrantUser/Controllers/SchoolMarkController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OptimalEducation.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OptimalEducation.Areas.EntrantUser.Controllers
+{
+	public class SchoolMarkController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
+		public UserManager<ApplicationUser> UserManager { get; private set; }
+
+		public SchoolMarkController()
+		{
+			UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+		}
+		public SchoolMarkController(UserManager<ApplicationUser> userManager)
+		{
+			UserManager = userManager;
+		}
+		// GET: /EntrantUser/SchoolMark/
+		public async Task<ActionResult> Index()
+		{
+			var examList = await GetUserSchoolMarkAsync();
+			return View(examList);
+		}
+        private async Task<List<SchoolMark>> GetUserSchoolMarkAsync()
+        {
+            var currentUser = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            //EntrantClusterizer cluser = new EntrantClusterizer(currentUser.Entrant);
+            var schoolMarks = db.SchoolMarks
+                .Include(u => u.SchoolDiscipline)
+                .Include(u => u.Entrant)
+                .Where(p => p.EntrantId == currentUser.EntrantId);
+
+            return await schoolMarks.ToListAsync();
+        }
+
+        // POST: /EntrantUser/UnitedStateExams/Index
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index([Bind(Include = "Result")] IEnumerable<SchoolMark> schoolMark)
+        {
+            var examList = await GetUserSchoolMarkAsync();
+            //Порядок не должен меняться, поэтому обновляем данные так
+            for (int i = 0; i < examList.Count; i++)
+            {
+                examList[i].Result = schoolMark.ElementAt(i).Result;
+            }
+            schoolMark = examList;//обновляем список новыми значениями(для передачи во View, в случае ошибки)
+
+            //Проверка 
+            if (ModelState.IsValid)
+            {
+                foreach (var exam in schoolMark)
+                {
+                    db.Entry(exam).State = EntityState.Modified;
+                }
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(schoolMark);
+        }
+
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
