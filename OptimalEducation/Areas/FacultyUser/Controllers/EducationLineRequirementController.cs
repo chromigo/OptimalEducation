ade3ffcaa9ccc95f20d368b0e11c2578a4c3556e
Eugene <chromigo@gmail.com>
2014-03-22 21:50:03 +0400

    Доделал возможность заполнять требования по экзаменам егэ, для учебных направлений. Заполнять можно у созданной записи в режиме редактирования.

--- OptimalEducation/Areas/FacultyUser/Controllers/EducationLineRequirementController.cs
+++ OptimalEducation/Areas/FacultyUser/Controllers/EducationLineRequirementController.cs
@@ -9,133 +9,190 @@ using System.Web;
 using System.Web.Mvc;
 using OptimalEducation.DAL.Models;
 using OptimalEducation.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace OptimalEducation.Areas.FacultyUser.Controllers
 {
-    [Authorize(Roles = Role.Faculty)]
-    public class EducationLineRequirementController : Controller
-    {
-        private OptimalEducationDbContext db = new OptimalEducationDbContext();
+	[Authorize(Roles = Role.Faculty)]
+	public class EducationLineRequirementController : Controller
+	{
+		private OptimalEducationDbContext db = new OptimalEducationDbContext();
+		private ApplicationDbContext dbIdentity = new ApplicationDbContext();
+		public UserManager<ApplicationUser> UserManager { get; private set; }
+		public EducationLineRequirementController()
+		{
+			UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(dbIdentity));
+		}
+		public EducationLineRequirementController(UserManager<ApplicationUser> userManager)
+		{
+			UserManager = userManager;
+		}
+		// GET: /FacultyUser/EducationLineRequirement/
+		public async Task<ActionResult> Index(int id)
+		{
+			var facultyId = await GetFacultyId();
+			var educationlinerequirements = db.EducationLineRequirements
+				.Include(e => e.ExamDiscipline)
+				.Where(e => e.EducationLineId == id && e.EducationLine.FacultyId==facultyId);
+			ViewBag.EducationLineId = id;
+			return View(await educationlinerequirements.ToListAsync());
+		}
 
-        // GET: /FacultyUser/EducationLineRequirement/
-        public async Task<ActionResult> Index(int id)
-        {
-            var educationlinerequirements = db.EducationLineRequirements
-                .Include(e => e.ExamDiscipline)
-                .Where(e=>e.Id==id);
-            return View(await educationlinerequirements.ToListAsync());
-        }
+		// GET: /FacultyUser/EducationLineRequirement/Details/5
+		public async Task<ActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			
+			EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
+            ViewBag.EducationLineId = educationlinerequirement.EducationLineId;
+			if (educationlinerequirement == null)
+			{
+				return HttpNotFound();
+			}
+			var facultyId = await GetFacultyId();
+			if (educationlinerequirement.EducationLine.FacultyId == facultyId)
+			{
+				return View(educationlinerequirement);
+			}
+			else return HttpNotFound();
+		}
 
-        // GET: /FacultyUser/EducationLineRequirement/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
-            if (educationlinerequirement == null)
-            {
-                return HttpNotFound();
-            }
-            return View(educationlinerequirement);
-        }
+		// GET: /FacultyUser/EducationLineRequirement/Create
+		public async Task<ActionResult> Create(int educationLineId)
+		{
+			ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name");
+			ViewBag.EducationLineId = educationLineId;
+			return View();
+		}
 
-        // GET: /FacultyUser/EducationLineRequirement/Create
-        public ActionResult Create()
-        {
-            ViewBag.EducationLineId = new SelectList(db.EducationLines, "Id", "Code");
-            ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name");
-            return View();
-        }
+		// POST: /FacultyUser/EducationLineRequirement/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create([Bind(Include="Requirement,EducationLineId,ExamDisciplineId")] EducationLineRequirement educationlinerequirement)
+		{
+			ViewBag.EducationLineId = educationlinerequirement.EducationLineId;
+			if (ModelState.IsValid)
+			{
+				var facultyId = await GetFacultyId();
+				var educationLine= await db.EducationLines.FindAsync(educationlinerequirement.EducationLineId);
 
-        // POST: /FacultyUser/EducationLineRequirement/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include="Id,Requirement,EducationLineId,ExamDisciplineId")] EducationLineRequirement educationlinerequirement)
-        {
-            if (ModelState.IsValid)
-            {
-                db.EducationLineRequirements.Add(educationlinerequirement);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+				if (educationLine.FacultyId == facultyId)
+				{
+					db.EducationLineRequirements.Add(educationlinerequirement);
+					await db.SaveChangesAsync();
+                    return RedirectToAction("Index", new { id = educationlinerequirement.EducationLineId });
+				}
+				else return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
 
-            ViewBag.EducationLineId = new SelectList(db.EducationLines, "Id", "Code", educationlinerequirement.EducationLineId);
-            ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name", educationlinerequirement.ExamDisciplineId);
-            return View(educationlinerequirement);
-        }
+			ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name", educationlinerequirement.ExamDisciplineId);
+			return View(educationlinerequirement);
+		}
 
-        // GET: /FacultyUser/EducationLineRequirement/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
-            if (educationlinerequirement == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.EducationLineId = new SelectList(db.EducationLines, "Id", "Code", educationlinerequirement.EducationLineId);
-            ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name", educationlinerequirement.ExamDisciplineId);
-            return View(educationlinerequirement);
-        }
+		// GET: /FacultyUser/EducationLineRequirement/Edit/5
+		public async Task<ActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
+            ViewBag.EducationLineId = educationlinerequirement.EducationLineId;
+			if (educationlinerequirement == null)
+			{
+				return HttpNotFound();
+			}
+			var facultyId = await GetFacultyId();
+			if (educationlinerequirement.EducationLine.FacultyId == facultyId)
+			{
+				ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name", educationlinerequirement.ExamDisciplineId);
+				return View(educationlinerequirement);
+			}
+			else return HttpNotFound();
+		}
 
-        // POST: /FacultyUser/EducationLineRequirement/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include="Id,Requirement,EducationLineId,ExamDisciplineId")] EducationLineRequirement educationlinerequirement)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(educationlinerequirement).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewBag.EducationLineId = new SelectList(db.EducationLines, "Id", "Code", educationlinerequirement.EducationLineId);
-            ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name", educationlinerequirement.ExamDisciplineId);
-            return View(educationlinerequirement);
-        }
+		// POST: /FacultyUser/EducationLineRequirement/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Edit([Bind(Include="Id,Requirement,EducationLineId,ExamDisciplineId")] EducationLineRequirement educationlinerequirement)
+		{
+			ViewBag.EducationLineId = educationlinerequirement.EducationLineId;
+			if (ModelState.IsValid)
+			{
+				var facultyId = await GetFacultyId();
+				var educationLine = await db.EducationLines.FindAsync(educationlinerequirement.EducationLineId);
+				if (educationLine.FacultyId==facultyId)
+				{
+					db.Entry(educationlinerequirement).State = EntityState.Modified;
+					await db.SaveChangesAsync();
+                    return RedirectToAction("Index", new { id = educationlinerequirement.EducationLineId });
+				}
+				else return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			ViewBag.ExamDisciplineId = new SelectList(db.ExamDisciplines, "Id", "Name", educationlinerequirement.ExamDisciplineId);
+			return View(educationlinerequirement);
+		}
 
-        // GET: /FacultyUser/EducationLineRequirement/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
-            if (educationlinerequirement == null)
-            {
-                return HttpNotFound();
-            }
-            return View(educationlinerequirement);
-        }
+		// GET: /FacultyUser/EducationLineRequirement/Delete/5
+		public async Task<ActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
+            ViewBag.EducationLineId = educationlinerequirement.EducationLineId;
+			if (educationlinerequirement == null)
+			{
+				return HttpNotFound();
+			}
+			var facultyId = await GetFacultyId();
+			if (educationlinerequirement.EducationLine.FacultyId == facultyId)
+			{
+				return View(educationlinerequirement);
+			}
+			else return HttpNotFound();
+		}
 
-        // POST: /FacultyUser/EducationLineRequirement/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
-            db.EducationLineRequirements.Remove(educationlinerequirement);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+		// POST: /FacultyUser/EducationLineRequirement/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> DeleteConfirmed(int id)
+		{
+			EducationLineRequirement educationlinerequirement = await db.EducationLineRequirements.FindAsync(id);
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+			var facultyId = await GetFacultyId();
+			if (educationlinerequirement.EducationLine.FacultyId == facultyId)
+			{
+				db.EducationLineRequirements.Remove(educationlinerequirement);
+				await db.SaveChangesAsync();
+                return RedirectToAction("Index", new { id = educationlinerequirement.EducationLineId });
+			}
+			else return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+		}
+		private async Task<int> GetFacultyId()
+		{
+			var currentUser = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+			var entrantClaim = currentUser.Claims.SingleOrDefault(p => p.ClaimType == MyClaimTypes.EntityUserId);
+			var entrantId = int.Parse(entrantClaim.ClaimValue);
+			return entrantId;
+		}
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+				dbIdentity.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
