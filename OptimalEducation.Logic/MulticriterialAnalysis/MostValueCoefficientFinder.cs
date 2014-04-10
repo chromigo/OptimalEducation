ba46ed7ace11bdc9fd256f11883986141955fbc7
Chromigo <Chromigo@gmail.com>
2014-04-10 18:28:52 +0400

    Добавил заготовки для метода многокритериального анализа: 1. Построекние отношений предпочтения для данного пользователя 2. Перестройка множества кластеров вузов по формуле
    Работоспособность не проверялась. Необходимо дописать/выделить в классы и написать нормальные тесты.

---
+++ OptimalEducation.Logic/MulticriterialAnalysis/MostValueCoefficientFinder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimalEducation.Logic.MulticriterialAnalysis
+{
+    public class MostValueCoefficientFinder
+    {
+        Dictionary<string, double> _userCluster;
+
+        Dictionary<string, double> _importantClusters;
+        Dictionary<string, double> _unImportantClusters;
+
+        public MostValueCoefficientFinder(Dictionary<string, double> userCluster)
+        {
+            _userCluster = userCluster;
+            SeparateClustersToImprotantAnd_UnImportant();
+        }
+        void SeparateClustersToImprotantAnd_UnImportant()
+        {
+            //Пусть пока работает по такому правилу:
+            //Находим максимальный критерий
+            var maxCluster = _userCluster.Max().Value;
+            //При предположении что все значения у нас от 0 до 1
+            //находим близкие по значению критерии - с разницей до -0,1
+            _importantClusters = (from cluster in _userCluster
+                                    where (cluster.Value >= (maxCluster-0.1))
+                                    select cluster).ToDictionary(p=>p.Key,el=>el.Value);
+
+            _unImportantClusters = (from cluster in _userCluster
+                                  where (cluster.Value < (maxCluster - 0.1))
+                                  select cluster).ToDictionary(p => p.Key, el => el.Value);
+        }
+        /// <summary>
+        /// Получаем отношение предпочтения
+        /// </summary>
+        /// <returns>Список из Важных кластеров и их отношений с неважными(teta)</returns>
+        public List<PreferenceRelation> GetPreferenceRelations()
+        {
+            var preferenceRelations = new List<PreferenceRelation>();
+            //Определяем коэффициенты отн важности. Здесь настраиваем правило, по которому будем строить эти коэффициенты
+            //На данный момент: задаем фиксир значения вручную(СМ TODO)
+            foreach (var impCluster in _importantClusters)
+            {
+                var preferenceRelation = new PreferenceRelation(impCluster.Key);
+                foreach (var unImpCluster in _unImportantClusters)
+                {
+                    //TODO: Определеить более клевую логику по выбору коэффициентов относительной важности
+
+                    //1. Через формулу относительной важности
+                    //var teta = TetaMethod();
+
+                    //2. Другой вариант - тупо попробовать использовать не формулу, а (1-значение значимости неважного коэффициента)
+                    var teta = 1 - unImpCluster.Value;
+
+                    //3. Etc
+                    //var teta = Wizzard.SuperMathFormula_Magic();
+
+                    preferenceRelation.Tetas.Add(unImpCluster.Key, teta);
+                }
+                preferenceRelations.Add(preferenceRelation);
+            }
+            return preferenceRelations;
+        }
+
+        private static double TetaMethod()
+        {
+            //На данный момент строятся одинаковые предпочтения для всех важных/неважных критериев
+            //готовы потерять 0.2 но получить 0.1
+            var w_j = 0.2;//сколько готовы пожертвовать(по незначимому коэф-ту)
+            var w_i = 0.1;//сколько хотим получить(по значимому коэф-ту)
+            var teta = w_j / (w_j + w_i);//Коэффициент относительной важности
+            //По идее все что <0.5 - означает что нам на самом деле ценне противоположный коэффициент
+            return teta;
+        }
+    }
+    public class PreferenceRelation
+    {
+        public PreferenceRelation(string name)
+        {
+            ImportantClusterName=name;
+        }
+        public string ImportantClusterName { get; private set; }
+        /// <summary>
+        /// Словарь "Название неважного кластера, Тета - коэффциент"
+        /// </summary>
+        public Dictionary<string, double> Tetas { get; set; }
+    }
+}
---
+++ UnitTests/MulticriterialAnalysisTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OptimalEducation.Logic.MulticriterialAnalysis;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MulticriterialAnalysisTest
+    {
+        Dictionary<string, double> _userCluster;
+
+        Dictionary<string, double> _universityCluster1;
+        Dictionary<string, double> _universityCluster2;
+        Dictionary<string, double> _universityCluster3;
+
+        List<Dictionary<string, double>> _totalUniversityClusters;
+        public MulticriterialAnalysisTest()
+        {
+            _userCluster = new Dictionary<string, double>();
+            _userCluster.Add("Русский", 0.5);
+            _userCluster.Add("Математика", 0.7);
+            _userCluster.Add("Информатика", 0.8);
+            _userCluster.Add("Физика", 0.4);
+
+            var _universityCluster1 = new Dictionary<string, double>();
+            _universityCluster1.Add("Русский", 0.5);
+            _universityCluster1.Add("Математика", 0.7);
+            _universityCluster1.Add("Информатика", 0.8);
+            _universityCluster1.Add("Физика", 0.4);
+            var _universityCluster2 = new Dictionary<string, double>();
+            _universityCluster2.Add("Русский", 0.4);
+            _universityCluster2.Add("Математика", 0.3);
+            _universityCluster2.Add("Информатика", 0.5);
+            _universityCluster2.Add("Физика", 0.4);
+            var _universityCluster3 = new Dictionary<string, double>();
+            _universityCluster3.Add("Русский", 0.8);
+            _universityCluster3.Add("Математика", 0.3);
+            _universityCluster3.Add("Информатика", 0.3);
+            _universityCluster3.Add("Физика", 0.1);
+
+            var _totalUniversityClusters = new List<Dictionary<string, double>>();
+            _totalUniversityClusters.Add(_universityCluster1);
+            _totalUniversityClusters.Add(_universityCluster2);
+            _totalUniversityClusters.Add(_universityCluster3);
+        }
+        //Здесь определяются предпочтения пользователя
+        //Можно напрямуе его опрашивать
+        //но т.к. хочется автоматизма, вывод о предпочтениях будем строить на основании информации из кластера пользователя
+        [TestMethod]
+        public void CreateNewParetto()
+        {
+            //Определяем группу важных критериев
+            var finder = new MostValueCoefficientFinder(_userCluster);
+            var userPrefer = finder.GetPreferenceRelations();
+
+            var recalculateUniversityClusters = new List<Dictionary<string, double>>();
+            foreach (var universityClusters in _totalUniversityClusters)
+            {
+                //Забиваем все кластеры теккущего вуза на важные и неважные
+                var importantClusters = new Dictionary<string, double>();
+                var unImportantClusters = new Dictionary<string, double>();
+                foreach (var cluster in universityClusters)
+                {
+                    if (userPrefer.Any(p => p.ImportantClusterName == cluster.Key))
+                        importantClusters.Add(cluster.Key, cluster.Value);
+                    else
+                        unImportantClusters.Add(cluster.Key, cluster.Value);
+                }
+
+                //Пересчитываем значения неважных кластеров
+                var recalculatedClusters = new Dictionary<string, double>();
+                foreach (var importantCluster in importantClusters)
+                {
+                    recalculatedClusters.Add(importantCluster.Key, importantCluster.Value);//Добавляем текущий значимый
+                    foreach (var unImportantCluster in unImportantClusters)
+                    {
+                        //формула пересчета
+                        var f_i = importantCluster.Value;
+                        var f_j = unImportantCluster.Value;
+
+                        var preference = userPrefer.Single(p => p.ImportantClusterName == importantCluster.Key);//находим значимый кластер в списке с предпочтениями
+                        var teta = preference.Tetas.Single(p => p.Key == unImportantCluster.Key).Value;//находим в этом кластере текущее отношение предпочтения
+
+                        var recalculatedCluster = teta * f_i + (1 - teta) * f_j;
+
+                        recalculatedClusters.Add(unImportantCluster.Key, recalculatedCluster);//добавляем пересчитанный
+                    }
+                }
+                recalculateUniversityClusters.Add(recalculatedClusters);
+            }
+
+            //Сужаем получившуюся таблицу - Паретто
+        }
+
+    }
+
+
+
+}
