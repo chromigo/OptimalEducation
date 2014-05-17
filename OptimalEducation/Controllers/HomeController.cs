bbe58916d2ec095063d24022cbdf5792f1e88b55
Eugene <chromigo@gmail.com>
2014-05-17 15:17:35 +0400

    fix

--- OptimalEducation/Controllers/HomeController.cs
+++ OptimalEducation/Controllers/HomeController.cs
@@ -18,13 +18,18 @@ namespace OptimalEducation.Controllers
             {
                 var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
 
-                var userRoles = await UserManager.GetRolesAsync(User.Identity.GetUserId());
-                if (userRoles.Any(role => role == Role.Admin))
-                    return RedirectToLocal("/Admin/Entrant/");
-                if (userRoles.Any(role => role == Role.Entrant))
-                    return RedirectToLocal("/EntrantUser/Info/");
-                if (userRoles.Any(role => role == Role.Faculty))
-                    return RedirectToLocal("/FacultyUser/Info/");
+                var userId = User.Identity.GetUserId();
+                
+                if(UserManager.Users.SingleOrDefault(p => p.Id == userId)!=null)
+                {
+                    var userRoles = await UserManager.GetRolesAsync(userId);
+                    if (userRoles.Any(role => role == Role.Admin))
+                        return RedirectToLocal("/Admin/Entrant/");
+                    if (userRoles.Any(role => role == Role.Entrant))
+                        return RedirectToLocal("/EntrantUser/Info/");
+                    if (userRoles.Any(role => role == Role.Faculty))
+                        return RedirectToLocal("/FacultyUser/Info/");
+                }
             }
             return View();
         }
