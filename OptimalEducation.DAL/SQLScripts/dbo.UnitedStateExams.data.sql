fcca658ce28413fba62f5b4601477b3bd78b70d0
Artem S <sawas-pro@yandex.ru>
2014-05-22 16:33:26 +0400

    sql для базы, скрипт автоматического заполнения, мелочи

--- OptimalEducation.DAL/SQLScripts/dbo.UnitedStateExams.data.sql
+++
@@ -1,6 +0,0 @@
-SET IDENTITY_INSERT [dbo].[UnitedStateExams] ON
-INSERT INTO [dbo].[UnitedStateExams] ([Id], [Result], [ExamDisciplineId], [EntrantId]) VALUES (7, 78, 1, 21)
-INSERT INTO [dbo].[UnitedStateExams] ([Id], [Result], [ExamDisciplineId], [EntrantId]) VALUES (8, 79, 2, 21)
-INSERT INTO [dbo].[UnitedStateExams] ([Id], [Result], [ExamDisciplineId], [EntrantId]) VALUES (9, 87, 3, 21)
-INSERT INTO [dbo].[UnitedStateExams] ([Id], [Result], [ExamDisciplineId], [EntrantId]) VALUES (10, 72, 6, 21)
-SET IDENTITY_INSERT [dbo].[UnitedStateExams] OFF
