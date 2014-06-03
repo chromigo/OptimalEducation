69f48a5f131253b44a804610a58d8c49853ef9ad
Eugene <chromigo@gmail.com>
2014-06-03 16:00:56 +0400

    Закомментировал сложные требования к паролю

--- OptimalEducation/App_Start/IdentityConfig.cs
+++ OptimalEducation/App_Start/IdentityConfig.cs
@@ -29,10 +29,10 @@ namespace OptimalEducation
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                //RequireNonLetterOrDigit = true,
+                //RequireDigit = true,
+                //RequireLowercase = true,
+                //RequireUppercase = true,
             };
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             // You can write your own provider and plug in here.
