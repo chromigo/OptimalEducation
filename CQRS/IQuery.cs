8ac825ed0974c0eecf759394cba99c9cdab55078
Eugene <chromigo@gmail.com>
2014-07-03 17:32:36 +0400

    Добавил библиотеку классов, основываясь на статье http://habrahabr.ru/post/125720/

---
+++ CQRS/IQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS
+{
+    /// <summary>
+    ///     Критерии запроса
+    /// </summary>
+    public interface ICriterion { }
+
+    public interface IQuery<in TCriterion, out TResult>
+      where TCriterion : ICriterion
+    {
+        TResult Ask(TCriterion criterion);
+    }
+
+    public interface IQueryFor<out T>
+    {
+       T With<TCriterion>(TCriterion criterion) where TCriterion : ICriterion;
+    }
+
+    public interface IQueryBuilder
+    {
+        IQueryFor<TResult> For<TResult>();
+    }
+}
---
+++ OptimalEducation/QueryBuilder.cs
@@ -0,0 +1,44 @@
+﻿using CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace OptimalEducation
+{
+    public class QueryBuilder : IQueryBuilder
+    {
+        private readonly IDependencyResolver dependencyResolver;
+
+        public QueryBuilder(IDependencyResolver dependencyResolver)
+        {
+            this.dependencyResolver = dependencyResolver;
+        }
+
+        public IQueryFor<TResult> For<TResult>()
+        {
+            return new QueryFor<TResult>(dependencyResolver);
+        }
+
+        #region Nested type: QueryFor
+
+        private class QueryFor<TResult> : IQueryFor<TResult>
+        {
+            private readonly IDependencyResolver dependencyResolver;
+
+            public QueryFor(IDependencyResolver dependencyResolver)
+            {
+                this.dependencyResolver = dependencyResolver;
+            }
+
+            public TResult With<TCriterion>(TCriterion criterion) where TCriterion : ICriterion
+            {
+                return dependencyResolver.GetService<IQuery<TCriterion, TResult>>().Ask(criterion);
+            }
+        }
+
+        #endregion
+    }
+}
