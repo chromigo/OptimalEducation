3e647c62a7a0271d4b091a9bd5aedd8f1479e2de
Eugene <chromigo@gmail.com>
2014-08-24 11:47:29 +0600

    Добавил тест класса вычисления расстояний

---
+++ OptimalEducation.UnitTests.Logic.DistanceRecomendator/CharacteristicDistanceTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptimalEducation.Implementation.Logic.DistanceRecomendator;
+using System.Collections.Generic;
+
+namespace OptimalEducation.UnitTests.Logic.DistanceRecomendator
+{
+    [TestClass]
+    public class CharacteristicDistanceTest
+    {
+        [TestMethod]
+        public void GetDistance_with_CityDistance_return_correct_answer()
+        {
+            //Arrange
+            var entrantCharacteristics =new Dictionary<string,double>();
+            var educationLineCharacteristics = new Dictionary<string, double>();
+
+            entrantCharacteristics.Add("Русский", 0.6);
+            entrantCharacteristics.Add("Математика", 0.7);
+            entrantCharacteristics.Add("Информатика", 0.7);
+
+            educationLineCharacteristics.Add("Русский", 0.5);
+            educationLineCharacteristics.Add("Математика", 0.7);
+            educationLineCharacteristics.Add("Информатика", 0.8);
+
+            var correctAnswer=Math.Abs(0.6-0.5)+Math.Abs(0.7-0.7)+Math.Abs(0.7-0.8);
+
+            //Act
+            var result = CharacteristicDistance.GetDistance(entrantCharacteristics, educationLineCharacteristics);
+
+            //Assert
+            Assert.AreEqual(correctAnswer, result);
+        }
+    }
+}
