c5d2067685ef8b0b3f6363039868d2ab91d4565b
Eugene <chromigo@gmail.com>
2014-08-24 15:35:34 +0600

    Добавил еще 1 тест для вычисления расстояния мд хараткеристиками

--- OptimalEducation.UnitTests.Logic.DistanceRecomendator/CharacteristicDistanceTest.cs
+++ OptimalEducation.UnitTests.Logic.DistanceRecomendator/CharacteristicDistanceTest.cs
@@ -31,5 +31,27 @@ namespace OptimalEducation.UnitTests.Logic.DistanceRecomendator
             //Assert
             Assert.AreEqual(correctAnswer, result);
         }
+
+        [TestMethod]
+        public void GetDistance_return_nullResult_if_in_Entrant_and_EducationLine_not_equal_key_elements()
+        {
+            //Arrange
+            var entrantCharacteristics = new Dictionary<string, double>();
+            var educationLineCharacteristics = new Dictionary<string, double>();
+
+            entrantCharacteristics.Add("Литература", 0.6);
+            entrantCharacteristics.Add("Математика", 0.7);
+            entrantCharacteristics.Add("Информатика", 0.7);
+
+            educationLineCharacteristics.Add("Русский", 0.5);
+            educationLineCharacteristics.Add("Математика", 0.7);
+            educationLineCharacteristics.Add("Информатика", 0.8);
+
+            //Act
+            var result = CharacteristicDistance.GetDistance(entrantCharacteristics, educationLineCharacteristics);
+
+            //Assert
+            Assert.IsTrue(!result.HasValue);
+        }
     }
 }
